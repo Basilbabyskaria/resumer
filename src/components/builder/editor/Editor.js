fc2730974807eeb8396d219882998320ede012e9
phoenix <basilbabyskaria@gmail.com>
2023-04-29 23:57:47 +0530

    .

--- src/components/builder/editor/Editor.js
+++ src/components/builder/editor/Editor.js
@@ -8,23 +8,63 @@ function Editor(props) {
     const basicInfoBody=(
         <div className={styles.details}>
             <div className={styles.row}>
-               
-                    <Input label="First Name"/>
-                    <Input label="Last Name"/>
-                    <Input label=" Email"/>
-                    <Input label="Phone No"/>
-                    <Input label="GitHub"/>
-                    <Input label="Linkidin"/>
-
-
-
-            
+                <div className={styles.col}>
+                        <Input label="First Name"/>
+                        <Input label="Last Name"/>
+                        
+                </div>
+                <Input label=" Email"/>
+                <Input label="Phone No"/>
+                <Input label="GitHub"/>
+                <Input label="Linkidin"/>
             </div>
 
         </div>
     );
+    const summaryBody=(
+        <div>
+            <textarea className={styles.textarea} cols="30" rows="10"></textarea>
+        </div>
+    );
+    const educationBody =(
+        <div>
+            
+        </div>
+    );
+    const workExpBody=(
+        <div>
 
+        </div>
+    );
+    const projectsBody =(
+        <div>
 
+        </div>
+    );
+    const achievementsBody =(
+        <div>
+
+        </div>
+    );
+    const othersBody=(
+        <div>
+
+        </div>
+    );
+
+    const generateBody=()=>{
+        switch (sections[activeSection]) {
+            case sections.basicInfo: return basicInfoBody;
+            case sections.summary: return summaryBody;
+            case sections.education: return educationBody;
+            case sections.work_experience: return workExpBody;
+            case sections.projects: return projectsBody;
+            case sections.achievements: return achievementsBody;
+            case sections.others: return othersBody;
+            default:return null;
+               
+        }
+    }
   return (
     <div className={styles.container}>
         <div className={styles.header}>
@@ -34,7 +74,7 @@ function Editor(props) {
         </div>
         <div className={styles.body}>
             <Input label="Title" placeholder="Enter section title"/>
-            {basicInfoBody}
+            {generateBody()}
         </div>
     </div>
   )
